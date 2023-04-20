e7bfbb2fadd45d5a975c64f2eb108e119606aa17
Bishwjeet kumar <66054602+bishwjeet7@users.noreply.github.com>
2023-04-20 17:25:05 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -25,8 +25,8 @@ export default function Home({session}) {
           <span className="span-tag">Sign in to your account</span>
           <div className="social">
             <div>
-              <a>
-                <button className="hover:bg-blue-500 hover:text-white" onClick={()=>signIn()}>
+              <a href="dashboard">
+                <button className="hover:bg-blue-500 hover:text-white">
                 <Image src={google} alt="google" />
                 <span>Sign in with Google</span>
                 </button>
